4c5e5a1daf9ab64c9ef730c44f98a74c960a4d3d
SantiagoGuzman <s.guzman.abregu@gmail.com>
2025-08-23 16:31:58 -0500

    Configuracion de entorno

---
+++ src/main/kotlin/com/dentista/dentista/DentistaApplication.kt
@@ -0,0 +1,11 @@
+package com.dentista.dentista
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DentistaApplication
+
+fun main(args: Array<String>) {
+	runApplication<DentistaApplication>(*args)
+}
