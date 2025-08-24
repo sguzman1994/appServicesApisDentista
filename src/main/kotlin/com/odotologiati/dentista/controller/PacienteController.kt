1e7a465321635ce014cc2d4a6500c0ceb3dc4ca6
SantiagoGuzman <s.guzman.abregu@gmail.com>
2025-08-23 23:22:09 -0500

    Pruebas Crud

--- src/main/kotlin/com/odotologiati/dentista/controller/PacienteController.kt
+++ src/main/kotlin/com/odotologiati/dentista/controller/PacienteController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/pacientes")
+@RequestMapping("/api/v1")
 @CrossOrigin("*")
 
 class PacienteController {
--- src/main/kotlin/com/odotologiati/dentista/model/Paciente.kt
+++ src/main/kotlin/com/odotologiati/dentista/model/Paciente.kt
@@ -1,9 +1,22 @@
 package com.odotologiati.dentista.model
 
-data class Paciente(
-    val id: Long,
-    val name: String,
-    val user: String,
-    val pass: String,
-    val email: String
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity(name = "paciente")
+class Paciente(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+    @Column
+    val name: String = "",
+    @Column
+    val user: String = "",
+    @Column
+    val pass: String = "",
+    @Column
+    val email: String = ""
 )
